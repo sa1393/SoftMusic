95beb86c2c20365b9fad6d04fd49abf8e2d06179
sa139339 <sa139339@gmail.com>
2021-04-12 23:02:13 +0900

    haha

---
+++ src/screens/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './home.css';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div className="home">         
+                
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ src/screens/musicBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './musicBar.css';
+
+import {Howl, Howler} from 'howler';
+
+ 
+
+class MusicBar extends React.Component {
+    render() {
+        return (
+            <div className="music_front_bar">
+            </div>
+        )
+    }
+}
+
+export default MusicBar;
