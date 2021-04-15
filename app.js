f4e6db1882d3fbbe63bd355469d8e690e8dec8f8
sa139339 <sa139339@gmail.com>
2021-04-15 23:07:33 +0900

    0415

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+  
+app.get("/", (req, res) => {
+  console.log("hahah");
+  res.redirect("/");
+});
+  
+const PORT = process.env.PORT || 8080;
+  
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.js';
+
+ReactDOM.render(
+  
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
