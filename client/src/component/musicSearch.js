bd5a45c8e20771aa7f8b7832cb9044f785b43dc1
unknown <sa139339@gmail.com>
2021-08-24 14:55:29 +0900

    검색창 수정

--- client/src/component/musicSearch.js
+++ client/src/component/musicSearch.js
@@ -6,24 +6,30 @@ import { faHome, faSearch, faChartLine } from '@fortawesome/free-solid-svg-icons
 import "../css/musicSearch.css";
 import DownloadButton from "./downloadButton";
 
-
-
 let MusicSearch = ()=>{
+    const [musicList, setMusicList] = useState(['1', '2', '3', '4']);
+    const [text, setText] = useState('');
 
-    useEffect(()=> {
-        console.log("11");
+    const onKeyPress=(e, value)=> {
+        if(e.key == 'Enter') {
+            searchMusic();
+        }
+    }
+
+    const searchMusic= (e)=> {
+        
         var request=require('request');
-
+        
         var optionParams={
-            q:"아무노래",
+            q:"",
             part:"snippet",
-            key:"AIzaSyCFYSD6tJOcz2YoB45nP7qZYKpTmD0YiE4",
-            maxResults:20,
+            key:"AIzaSyDXRkhYhznJVHsazwLBwPVz9pqR-u1zVRU",
+            maxResults:2,
             type:"video",
-            maxResults:10,
             regionCode:"KR",
             videoDuration:"short"
         };
+        optionParams.q = text;
 
         optionParams.q=encodeURI(optionParams.q);
 
@@ -35,32 +41,42 @@ let MusicSearch = ()=>{
         url=url.substr(0, url.length-1);
 
         request(url, function(err, res, body){
-            // console.log(body)
             
+            setMusicList([]);
             //json형식을 서버로 부터 받음
             var data=JSON.parse(body).items;
             for(var content in data){
-                
                 //youtube downloader에 videoId 넘기면 됨.
                 // console.log(data[content].snippet.title+" : "+data[content].id.videoId);
-                console.log(data[content]);
+                musicList.push(data[content]);
             }
         });
-    },[])
+        
+    }
 
+    const ChangeText = (e) => {
+        setText(e.target.value);
+    }
+    
     return(
         <div className="search">
             <div className="searchBarArea">
                 <FontAwesomeIcon icon={faSearch} className="searchIcon"></FontAwesomeIcon>
-                <input type="text" className="searchBar"></input>
+                <input type="text" value={text} onChange={ChangeText} className="searchBar" onKeyPress={onKeyPress}></input>
             </div>
             <div className="resultMusic">
+                {
+                    musicList.map((data, idx) => {
+                        console.log(data);
+                        return data;
+                    })
+                }
+                {/* <DownloadButton id="UuV2BmJ1p_I" type="mp3" />
                 <DownloadButton id="UuV2BmJ1p_I" type="mp3" />
                 <DownloadButton id="UuV2BmJ1p_I" type="mp3" />
                 <DownloadButton id="UuV2BmJ1p_I" type="mp3" />
                 <DownloadButton id="UuV2BmJ1p_I" type="mp3" />
-                <DownloadButton id="UuV2BmJ1p_I" type="mp3" />
-                <DownloadButton id="UuV2BmJ1p_I" type="mp3" />
+                <DownloadButton id="UuV2BmJ1p_I" type="mp3" /> */}
             </div>
         </div>
     )
